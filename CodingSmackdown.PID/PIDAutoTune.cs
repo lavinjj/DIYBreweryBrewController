34defdd7435bbba1d336913fae67d27e458f4813
Jim Lavin <jlavin@jimlavin.net>
2012-06-13 21:33:23 -0500

    Completed porting Arduino PID Auto Tune Library

--- CodingSmackdown.PID/PIDAutoTune.cs
+++ CodingSmackdown.PID/PIDAutoTune.cs
@@ -40,5 +40,260 @@ namespace CodingSmackdown.PID
 {
     public class PIDAutoTune
     {
+        private bool _isMax;
+        private bool _isMin;
+        private double _setpoint;
+        private bool _running;
+        private long _peak1;
+        private long _peak2;
+        private long _lastTime;
+        private int _sampleTime;
+        private int _lookBack;
+        private int _peakType;
+        private double[] _lastInputs;
+        private double[] _peaks;
+        private int _peakCount;
+        private bool _justchanged;
+        private double _absMax;
+        private double _absMin;
+        private double _outputStart;
+        private double _ku;
+        private double _pu;
+
+        public enum ControllerType
+        {
+            PI = 0,
+            PID = 1
+        }
+
+        public PIDAutoTune()
+        {
+            _lastInputs = new double[100];
+            _peaks = new double[10];
+        }
+
+        /// <summary>
+        /// Similar to the PID Compue function, returns non 0 when done
+        /// </summary>
+        /// <returns></returns>
+        public int Runtime()
+        {
+            if (_peakCount > 9 && _running)
+            {
+                _running = false;
+                FinishUp();
+                return 1;
+            }
+
+            long now = DateTime.Now.Ticks;
+
+            if ((now - _lastTime) < _sampleTime)
+            {
+                return 0;
+            }
+
+            _lastTime = now;
+
+            double refVal = Input;
+
+            if (!_running)
+            {
+                //initialize working variables the first time around
+                _peakType = 0;
+                _peakCount = 0;
+                _justchanged = false;
+                _absMax = refVal;
+                _absMin = refVal;
+                _setpoint = refVal;
+                _running = true;
+                _outputStart = Output;
+                Output = _outputStart + OutputStep;
+            }
+            else
+            {
+                if (refVal > _absMax)
+                {
+                    _absMax = refVal;
+                }
+
+                if (refVal < _absMin)
+                {
+                    _absMin = refVal;
+                }
+            }
+
+            //oscillate the output based on the input's relation to the setpoint
+            if (refVal > _setpoint + NoiseBand)
+            {
+                Output = _outputStart - OutputStep;
+            }
+            else if (refVal < _setpoint - NoiseBand)
+            {
+                Output = _outputStart + OutputStep;
+            }
+
+            _isMax = true;
+            _isMin = true;
+
+            //id peaks
+            for (int i = _lookBack - 1; i >= 0; i--)
+            {
+                double val = _lastInputs[i];
+
+                if (_isMax)
+                {
+                    _isMax = refVal > val;
+                }
+
+                if (_isMin)
+                {
+                    _isMin = refVal < val;
+                }
+
+                _lastInputs[i + 1] = _lastInputs[i];
+            }
+
+            _lastInputs[0] = refVal;
+
+            if (_lookBack < 9)
+            {
+                //we don't want to trust the maxes or mins until the inputs array has been filled
+                return 0;
+            }
+
+            if (_isMax)
+            {
+                if (_peakType == 0)
+                {
+                    _peakType = 1;
+                }
+
+                if (_peakType == -1)
+                {
+                    _peakType = 1;
+                    _justchanged = true;
+                    _peak2 = _peak1;
+                }
+
+                _peak1 = now;
+
+                _peaks[_peakCount] = refVal;
+
+            }
+            else if (_isMin)
+            {
+                if (_peakType == 0)
+                {
+                    _peakType = -1;
+                }
+
+                if (_peakType == 1)
+                {
+                    _peakType = -1;
+                    _peakCount++;
+                    _justchanged = true;
+                }
+
+                if (_peakCount < 10) _peaks[_peakCount] = refVal;
+            }
+
+            if (_justchanged && _peakCount > 2)
+            { 
+                //we've transitioned.  check if we can autotune based on the last peaks
+                double avgSeparation = (System.Math.Abs(_peaks[_peakCount - 1] - _peaks[_peakCount - 2]) + System.Math.Abs(_peaks[_peakCount - 2] - _peaks[_peakCount - 3])) / 2;
+
+                if (avgSeparation < 0.05 * (_absMax - _absMin))
+                {
+                    FinishUp();
+                    _running = false;
+                    return 1;
+                }
+            }
+
+            _justchanged = false;
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Stops the AutoTune
+        /// </summary>
+        public void Cancel()
+        {
+            _running = false;
+        }
+
+        public double Input { get; set; }
+
+        public double Output { get; set; }
+
+        /// <summary>
+        /// how far above and below the starting value will the output step?
+        /// </summary>
+        /// <param name="?"></param>
+        public double OutputStep { get; set; }
+        /// <summary>
+        /// Determies if the tuning parameters returned will be PI (D=0)
+        /// or PID.  (0=PI, 1=PID)			
+        /// </summary>
+        public ControllerType ControlType { get; set; }
+
+        /// <summary>
+        /// how far back are we looking to identify peaks
+        /// </summary>
+        public int LookbackSeconds
+        {
+            get
+            {
+                return _lookBack * _sampleTime / 1000;
+            }
+
+            set
+            {
+                if (value < 1) value = 1;
+
+                if (value < 25)
+                {
+                    _lookBack = value * 4;
+                    _sampleTime = 250;
+                }
+                else
+                {
+                    _lookBack = 100;
+                    _sampleTime = value * 10;
+                }
+            }
+        }
+
+        /// <summary>
+        /// the autotune will ignore signal chatter smaller than this value
+        /// this should be acurately set
+        /// </summary>
+        public double NoiseBand { get; set; }
+
+        public double Kp
+        {
+            get { return ControlType == ControllerType.PID ? 0.6 * _ku : 0.4 * _ku; }
+        }
+
+        public double Ki
+        {
+            // Ki = Kc/Ti 
+            get { return ControlType == ControllerType.PID ? 1.2 * _ku / _pu : 0.48 * _ku / _pu; }
+        }
+
+        //Kd = Kc * Td
+        public double Kd
+        {
+            get { return ControlType == ControllerType.PID ? 0.075 * _ku * _pu : 0; }
+        }
+
+        private void FinishUp()
+        {
+            Output = _outputStart;
+            //we can generate tuning parameters!
+            _ku = 4 * (2 * OutputStep) / ((_absMax - _absMin) * 3.14159);
+            _pu = (double)(_peak1 - _peak2) / 1000;
+        }
     }
 }
--- CodingSmackdown.Services/TemperatureControlService.cs
+++ CodingSmackdown.Services/TemperatureControlService.cs
@@ -18,9 +18,21 @@ namespace CodingSmackdown.Services
 
         private Thermistor _thermistor = null;
         private PIDController _pid = null;
-        private double _output = 0;
+        private PIDAutoTune _pidAutoTune = null;
+
+        private double _output = 50;
         private int _windowSize = 5000;
         private long _windowStartTime;
+        private double _kp = 2;
+        private double _ki = 0.5;
+        private double _kd = 2;
+        private double _aTuneStep=50;
+        private double _aTuneNoise=1;
+        private double _aTuneStartValue=100;
+        private int _aTuneLookBack = 20;
+        private bool _tuning = true;
+        private PIDController.PID_Mode _autoTuneModeRemember = PIDController.PID_Mode.AUTOMATIC;
+
 
         public TemperatureControlService(OutputHelper helper)
         {
@@ -30,7 +42,7 @@ namespace CodingSmackdown.Services
             _thermistor.VoltageReference = 3.3f;
             _thermistor.ResistanceReference = 1470000;
 
-            _pid = new PIDController(PinManagement.currentTemperatureSensor, _output, PinManagement.setTemperature, 2, 5, 1, PIDController.PID_Direction.DIRECT);
+            _pid = new PIDController(PinManagement.currentTemperatureSensor, _output, PinManagement.setTemperature, _kp, _ki, _kd, PIDController.PID_Direction.DIRECT);
 
             _windowSize = (int)SystemSettings.MinutesBetweenReadings;
 
@@ -39,6 +51,42 @@ namespace CodingSmackdown.Services
             _pid.Mode = PIDController.PID_Mode.AUTOMATIC;
 
             _windowStartTime = DateTime.Now.Ticks;
+
+            _pidAutoTune = new PIDAutoTune();
+
+            if (_tuning)
+            {
+                _tuning = false;
+
+                ChangeAutoTune();
+
+                _tuning = true;
+            }
+        }
+
+        private void ChangeAutoTune()
+        {
+            if (!_tuning)
+            {
+                //Set the output to the desired starting frequency.
+                _output = _aTuneStartValue;
+                _pidAutoTune.NoiseBand = _aTuneNoise;
+                _pidAutoTune.OutputStep = _aTuneStep;
+                _pidAutoTune.LookbackSeconds = _aTuneLookBack;
+
+                AutoTuneHelper(true);
+                
+                _tuning = true;
+            }
+            else
+            { 
+                //cancel autotune
+                _pidAutoTune.Cancel();
+
+                _tuning = false;
+
+                AutoTuneHelper(false);
+            }
         }
         
         protected override void Run()
@@ -55,12 +103,38 @@ namespace CodingSmackdown.Services
                     PinManagement.currentTemperatureSensor = (float)tempFahrenheit;
                     PinManagement.temperatureCelsiusSensor = (float)tempCelsius;
 
-                    _pid.Input = PinManagement.currentTemperatureSensor;
-                    _pid.SetPoint = PinManagement.setTemperature;
+                    if (_tuning)
+                    {
+                        _pidAutoTune.Input = PinManagement.currentTemperatureSensor;
 
-                    _pid.Compute();
+                        int val = (_pidAutoTune.Runtime());
 
-                    _output = _pid.Output;
+                        if (val != 0)
+                        {
+                            _tuning = false;
+                        }
+                        
+                        if (!_tuning)
+                        { //we're done, set the tuning parameters
+                            _kp = _pidAutoTune.Kp;
+                            _ki = _pidAutoTune.Ki;
+                            _kd = _pidAutoTune.Kd;
+                            _pid.SetTunings(_kp, _ki, _kd);
+
+                            AutoTuneHelper(false);
+                        }
+
+                        _output = _pidAutoTune.Output;
+                    }
+                    else
+                    {
+                        _pid.Input = PinManagement.currentTemperatureSensor;
+                        _pid.SetPoint = PinManagement.setTemperature;
+
+                        _pid.Compute();
+
+                        _output = _pid.Output;
+                    }
 
                     if (PinManagement.heaterEngaged)
                     {
@@ -71,7 +145,9 @@ namespace CodingSmackdown.Services
 
                         if (_output < (DateTime.Now.Ticks - _windowStartTime))
                         {
+                            // turn the heating element on
                             PinManagement.heaterOnOffPort.Write(true);
+                            // set our internal flag
                             PinManagement.isHeating = true;
                             // display heat is on
                             _outputHelper.DisplayText("Heat On");
@@ -100,6 +176,18 @@ namespace CodingSmackdown.Services
 
                 // Give up the clock so that the other threads can do their work
                 Thread.Sleep(10);
+            }
+        }
+
+        private void AutoTuneHelper(bool start)
+        {
+            if (start)
+            {
+                _autoTuneModeRemember = _pid.Mode;
+            }
+            else
+            {
+                _pid.Mode = _autoTuneModeRemember;
             }
         }
     }
